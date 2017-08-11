eecccf86490436125e5185c6b858102efe3d64d3
Jeff Wolfe <jeff@wolfeplanet.net>
2017-08-10 23:07:42 -0400

    added google reminders

--- resources/views/homeDraftDay.blade.php
+++ resources/views/homeDraftDay.blade.php
@@ -1,9 +1,17 @@
 <div class="col-md-12">
 	<div class="text-center lead">2017 Draft Day</div>
-	<div class="text-center">Saturday, August 26 at 2pm.
+	<div class="text-center">Saturday, August 26 at 12pm.
 	<p><a href="http://www.mtffl.com/smackroom/viewtopic.php?f=1&t=2580">Driving Directions</a></p>
+	<p>Click to add a reminder with address <a target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=MDc2azBzOTQ2bGkwZGdyZnJscGxsMXZ2b2ggd29sZmVwbGFuZXRAbQ&amp;tmsrc=wolfeplanet%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"></a>
+	</p>
 	</div>
+	<div>&nbsp;</div>
+	<div>&nbsp;</div>
 	<div class="text-center">Freeze Deadline: Friday, August 18, 11pm CT.</div>
+	<div class="text-center">Click for to add a reminder 
+<a target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=NzBpb3Q2MjVsZ3FtZDNnbHJsMWU4a2lnNG8gd29sZmVwbGFuZXRAbQ&amp;tmsrc=wolfeplanet%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"></a></div>
+
+	<div>&nbsp;</div>
 	<div class="text-center"><a href="https://www62.myfantasyleague.com/2017/options?L=42559&O=187">Click</a> to freeze your players.</div>
 	<div class="text-center">Discuss here: <a href="http://www.mtffl.com/smackroom/viewtopic.php?f=1&t=2615">http://mtffl.com/smackroom/</a></div>
 </div>
