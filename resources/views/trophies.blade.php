eef9989d975cf731ae00ae1d72c268b7d6078aa2
jdwolfe <jeff@wolfeplanet.net>
2017-08-27 09:48:41 -0500

    added trophy page with images

--- resources/views/trophies.blade.php
+++ resources/views/trophies.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 
-Trophies
+<h1>Trophies</h1>
+
+<div>
+	<h2>Champion Trophy Cup</h2>
+	<img src="/images/champion-cup.png" alt="MTFFL Champion Cup" Title="MTFFL Champion Cup" />
+</div>
+
+<div>
+	<h2>The Cup</h2>
+	<img src="/images/the-cup-front.png" alt="MTFFL The Cup" Title="MTFFL The Cup" />
+</div>
+
+<div>
+	<h2>The Cup Stand</h2>
+	<img src="/images/trophy-stand-1.png" alt="MTFFL Trophy Stand" Title="MTFFL Trophy Stand" />
+</div>
 
 @endsection
