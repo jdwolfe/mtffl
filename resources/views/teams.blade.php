7f7aaa5ca298cc96036c4f1d1c26a236ab5cbd86
jdwolfe <jeff@wolfeplanet.net>
2019-10-17 17:40:28 -0500

    white space fixes

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -2,28 +2,30 @@
 
 @section('content')
 
+<div>
 <h1>MTFFL Teams</h1>
 <ul>
 <?php $division = ''; ?>
-	@foreach( $teams as $t )
-		<?php if( $division != $t->division ) {
-			$division = $t->division;
-		?>
+@foreach( $teams as $t )
+	<?php if( $division != $t->division ) {
+		$division = $t->division;
+	?>
+	</ul>
+	<h2>{{ $division }}</h2>
+	<ul>
+	<?php
+	}
+	?>
+		<li><a href="{{ url('team/info/' . $t->team_id) }}">{{ $t->team_longname }}</a>
+		@if( 'active' == $t->status )
+			(est. {{ $t->member_since }})
+		@else
+			({{ $t->member_since }} - {{ $t->retired_from_league }})
+		@endif
+		</li>
+@endforeach
 </ul>
-<h2>{{ $division }}</h2>
-<ul>
-		<?php
-		}
-		?>
-	<li><a href="{{ url('team/info/' . $t->team_id) }}">{{ $t->team_longname }}</a>
-	@if( 'active' == $t->status )
-		(est. {{ $t->member_since }})
-	@else
-		({{ $t->member_since }} - {{ $t->retired_from_league }})
-	@endif
-	</li>
-	@endforeach
-</ul>
+</div>
 
 <div>
 	<a href="{{ url('teams/all') }}">Show Retired Teams</a>
